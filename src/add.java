b8aa22fabf275d436d292f0d6266a3116d870070
Yash Patil <patilyashyp12@gmail.com>
2025-06-01 13:25:30 +0530

    Initial commit

---
+++ src/add.java
@@ -0,0 +1,8 @@
+public class add {
+
+    public static void main(String[] args){
+
+        System.out.println("Hello wrorld ");
+
+    }
+}
---
+++ src/lec2.java
@@ -0,0 +1,6 @@
+public class lec2 {
+
+    public static void main (String args[]){
+        System.out.println("Hello world ");
+    }
+}
